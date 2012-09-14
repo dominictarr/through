63b41c6e073998cd8ed79a729339b1a728be088d
Dominic Tarr <dominic.tarr@gmail.com>
2012-09-15 00:58:17 +0200

    optional buffering

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var Stream = require('stream')
 // a stream that does nothing but re-emit the input.
 // useful for aggregating a series of changing but not ending streams into one stream)
 
+
+
 exports = module.exports = through
 through.through = through
 
@@ -15,16 +17,36 @@ function through (write, end) {
   end = end || function () { this.emit('end') }
 
   var ended = false, destroyed = false
-  var stream = new Stream()
+  var stream = new Stream(), buffer = []
+  stream.buffer = buffer
   stream.readable = stream.writable = true
   stream.paused = false  
   stream.write = function (data) {
     write.call(this, data)
     return !stream.paused
   }
+
+  function drain() {
+    while(buffer.length && !stream.paused) {
+      var data = buffer.shift()
+      if(null === data)
+        return stream.emit('end')
+      else
+        stream.emit('data', data)
+    }
+  }
+
+  stream.queue = function (data) {
+    buffer.push(data)
+    drain()
+  }
+
   //this will be registered as the first 'end' listener
   //must call destroy next tick, to make sure we're after any
   //stream piped from here. 
+  //this is only a problem if end is not emitted synchronously.
+  //a nicer way to do this is to make sure this is the last listener for 'end'
+
   stream.on('end', function () {
     stream.readable = false
     if(!stream.writable)
@@ -33,24 +55,31 @@ function through (write, end) {
       })
   })
 
+  function _end () {
+    stream.writable = false
+    end.call(stream)
+    if(!stream.readable)
+      stream.destroy()
+  }
+
   stream.end = function (data) {
     if(ended) return 
     //this breaks, because pipe doesn't check writable before calling end.
     //throw new Error('cannot call end twice')
     ended = true
     if(arguments.length) stream.write(data)
-    this.writable = false
-    end.call(this)
-    if(!this.readable)
-      this.destroy()
+    if(!buffer.length) _end()
   }
+
   stream.destroy = function () {
     if(destroyed) return
     destroyed = true
     ended = true
+    buffer.length = 0
     stream.writable = stream.readable = false
     stream.emit('close')
   }
+
   stream.pause = function () {
     if(stream.paused) return
     stream.paused = true
@@ -60,7 +89,11 @@ function through (write, end) {
     if(stream.paused) {
       stream.paused = false
     }
-    stream.emit('drain')
+    drain()
+    //may have become paused again,
+    //as drain emits 'data'.
+    if(!stream.paused)
+      stream.emit('drain')
   }
   return stream
 }
