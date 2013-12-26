69d31f3efe1600a44787cb198d7b97ae7eaa7167
Dominic Tarr <dominic.tarr@gmail.com>
2013-12-26 16:10:03 +0700

    remove concat-stream

--- test/async.js
+++ test/async.js
@@ -1,12 +1,11 @@
 var from = require('from')
 var through = require('../')
-var concat = require('concat-stream')
 
 var tape = require('tape')
 
 tape('simple async example', function (t) {
  
-  var n = 0, expected = [1,2,3,4,5]
+  var n = 0, expected = [1,2,3,4,5], actual = []
   from(expected)
   .pipe(through(function(data) {
     this.pause()
@@ -14,17 +13,16 @@ tape('simple async example', function (t) {
     setTimeout(function(){
       console.log('pushing data', data)
       this.push(data)
-//      if(!--n) this.resume()
       this.resume()
     }.bind(this), 300)
   })).pipe(through(function(data) {
     console.log('pushing data second time', data);
     this.push(data)
-  })).pipe(concat(function(result) {
-    console.log("voila", result)
-
-    t.deepEqual(result, expected)
+  })).on('data', function (d) {
+    actual.push(d)
+  }).on('end', function() {
+    t.deepEqual(actual, expected)
     t.end()
-  }))
+  })
 
 })
