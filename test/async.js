d7f659d672389fd59b5e5a1cbae61b397a3ec175
Joe Wagner <n.joseph.wagner@gmail.com>
2018-07-26 18:20:13 -0600

    Add failing Readable Stream async test

--- test/async.js
+++ test/async.js
@@ -1,4 +1,5 @@
 var from = require('from')
+var Readable = require('stream').Readable
 var through = require('../')
 
 var tape = require('tape')
@@ -24,5 +25,27 @@ tape('simple async example', function (t) {
     t.deepEqual(actual, expected)
     t.end()
   })
+
+})
+
+tape('does not end when paused with Readable Stream', function (t) {
+  var rs = new Readable
+  var expected = ['1','2','3','4','5'], actual = []
+
+  expected.forEach(data => rs.push(data))
+  rs.push(null)
+
+  rs.pipe(through(function(dataBuffer) {
+    var data = dataBuffer.toString()
+    this.pause()
+    setTimeout(function(){
+      console.log('pushing data', data)
+      actual.push(data)
+      this.resume()
+    }.bind(this), 300)
+  }, function() {
+    t.deepEqual(actual, expected)
+    t.end()
+  }))
 
 })
